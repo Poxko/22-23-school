a76de3d2dd660298da0666578590690126345167
Antonio Solare <80260208+Poxko@users.noreply.github.com>
2023-03-01 13:00:33 +0100

    aggiunta file

---
+++ Python/for.py
@@ -0,0 +1,45 @@
+a = int(input("Inserisci quanti numeri vuoi inserire: "))
+lista=[]
+
+for i in range(0,a):
+    pippo = int(input("Inserisci il numero: "))
+    lista.append(pippo)
+
+
+print("STAMPA LISTA")
+print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
