b71601d81b851ff429ba9a58d1742018ebfe6769
Antonio Solare <80260208+Poxko@users.noreply.github.com>
2023-03-20 10:54:17 +0100

    manutenzione

--- Python/figura_geometrica.py
+++ Python/figura_geometrica.py
@@ -1,12 +1,8 @@
 import turtle
 t= turtle.Screen()
 t = turtle.Turtle()
-
 num = int(input('Inserisci il numero di lati della figura: '))
 s=((num-2)*180/num)-180
-
-t.forward(100)
-
-for i in range(num-1):
+for i in range(num):
     t.right(s)
     t.forward(100)
